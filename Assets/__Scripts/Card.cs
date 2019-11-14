205481fe7349180870473f1808288c356dd13a81
Jayvious <JayviousWilliams@gmail.com>
2019-11-14 00:59:34 -0500

    more work

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -14,26 +14,73 @@ public class Card : MonoBehaviour {
 	
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
+    public SpriteRenderer[] spriteRenderers;
 
-
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
-
-		set {
-			back.SetActive(!value);
-		}
-	}
 
 
 	// Use this for initialization
 	void Start () {
-	
+        SetSortOrder(0);
 	}
 	
-	// Update is called once per frame
-	void Update () {
+    public void PopulateSpriteRenders()
+    {
+        if(spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenders();
+
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+   public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenders();
+
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            if(tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 	
 	}
 } // class Card
---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum eCardSate
+{
+    drawpile,
+    tableau,
+    target,
+    discard
+
+}
+public class CardProspector : Card
+{
+    [Header("Set Dynamically: CardPospector")]
+    public eCardSate state = eCardSate.drawpile;
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    public int layoutID;
+    public SlotDef slotDef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
