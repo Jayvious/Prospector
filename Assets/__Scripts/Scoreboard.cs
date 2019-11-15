82beb9b6435a5521d7917322d99d6987d0ad0b71
Jayvious <54966469+Jayvious@users.noreply.github.com>
2019-11-14 22:39:09 -0500

    another push

---
+++ Assets/__Scripts/Scoreboard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    public static Scoreboard S;
+    [Header("Set in Inspector")]
+    public GameObject prefabFloatingScore;
+
+    [Header("Set Dynamically")]
+    [SerializeField] private int _score = 0;
+    [SerializeField] private string _scoreString;
+
+    private Transform canvasTrans;
+
+    public int score
+    {
+        get
+        {
+            return (_score);
+        }
+        set
+        {
+            _score = value;
+            _scoreString = _score.ToString("NO");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
